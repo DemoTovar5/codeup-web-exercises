8febb8aac6169f9717eead850c5d159a3fb9c1e7
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-04-15 16:08:38 -0500

    add in files

---
+++ JS/Javascript-exercises.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//-------PRIMITIVE TYPES EXERCISES----
+// console.log(99.9);
+// console.log("false");
+// console.log(false);
+// console.log('0');
+// console.log(0);
+// console.log(true);
+// console.log('true');
+
+//------OPERATOR EXERCISES-----
+// console.log('1' + 2);
+// console.log(typeof '1' + 2);
+// console.log(6 % 4);
+// console.log('3 + 4 is ' + (3 + 4));
+// console.log(0 < 0);
+// console.log('false' == false);
+// console.log(true == 'true');
+// console.log(5 >= -5);
+// console.log(!false || false);
+// console.log(true || "42");
+// console.log(!true && !false);
+// console.log(5 < 4 && 1 === 1);
+// console.log(typeof 'codeup' === 'string');
+// console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+// console.log(4 >= 0 && 1 !== '1');
+// console.log(6 % 3 === 0);
+// console.log(5 % 2 !== 0);
