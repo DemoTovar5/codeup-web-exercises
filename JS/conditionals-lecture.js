a1a0cdc7438f1c24891acd0e4f68fa502ea12505
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-04-19 14:05:28 -0500

    04-19-21

---
+++ JS/conditionals-lecture.js
@@ -0,0 +1,69 @@
+ (function() {
+     "use strict";
+//      //
+//      //    var hasCar = false;
+//      // if(hasCar){
+//      //     console.log("Let's get in and go!");
+//      // }else{
+//      //     console.log("We need to call an Uber!");
+//      // }
+
+        //  function getToDestination(age, isInsured, hasCar, canGetRideshare){
+        //      var isLicensed = age >= 16;
+        //      var isLegalDriver = isLicensed && isInsured;
+        //
+        //
+        //
+        //
+        //      if (isLegalDriver && hasCar){
+        //          console.log("Let's get in and go!");
+        //      } else if(!isLegalDriver && canGetRideshare){
+        //          console.log("Let's call a rideshare!");
+        //      } else{
+        //          console.log("I think my friend can give us a lift!");
+        //      }
+        //  }
+        //
+        //  function canGetRideshare(){
+        //
+        //
+        //      var funds = true;
+        //      var DriversNearby = true;
+        //
+        //     if (funds === true && DriversNearby === true){
+        //         console.log("Driver is on the way!");
+        //     } else{
+        //         console.log("Driver not available.");
+        //     }
+        //
+        //
+        //  }
+        //
+        // getToDestination(15, false ,false,canGetRideshare());
+
+
+function isCorrectFruit(fruit){
+
+      switch(fruit){//string
+
+          case "apple": //string
+              console.log("This is an apple");
+              break;
+          case "orange":
+              console.log("This is an orange");
+              break;
+          case "banana":
+              console.log("This is a banana");
+              break;
+          default:
+              console.log("Idk probs not a fruit");
+
+      }
+
+      console.log()
+
+}
+
+
+ }
+ )()
--- JS/the-great-escape.js
+++ JS/the-great-escape.js
@@ -37,3 +37,4 @@ function playTheGreatEscape(){
 }
 
 playTheGreatEscape();
+
