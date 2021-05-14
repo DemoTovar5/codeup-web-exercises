e9977ba8e0f30e707f1f15853754c1e19d2f9e16
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-05-14 11:31:28 -0500

    MAY 14TH

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,26 +1,26 @@
-function showMultiplicationTable(num) {
+// function showMultiplicationTable(num) {
+//
+//     for (let i = 1; i <= 10; i++) {
+//
+//        console.log((num + "x" + i + " = " + i * num));
+//     }
+// }
+//
+// showMultiplicationTable(8);
 
-    for (let i = 1; i <= 10; i++) {
-
-       console.log((num + "x" + i + " = " + i * num));
-    }
-}
-
-showMultiplicationTable(8);
-
-function randomNumber() {
-
-    for (var i = 1; i <= 10; i++) {
-        let randomNum = Math.floor(Math.random() * (200 - 20) + 20);
-        if (randomNum % 2 === 0) {
-            console.log(randomNum + " is even");
-        } else {
-          console.log(randomNum + " is odd");
-        }
-    }
-}
-
-randomNumber();
+// function randomNumber() {
+//
+//     for (var i = 1; i <= 10; i++) {
+//         let randomNum = Math.floor(Math.random() * (200 - 20) + 20);
+//         if (randomNum % 2 === 0) {
+//             console.log(randomNum + " is even");
+//         } else {
+//           console.log(randomNum + " is odd");
+//         }
+//     }
+// }
+//
+// randomNumber();
 
 function numberTree() {
     for (var i = 1; i <= 9; i++) {
@@ -34,11 +34,11 @@ let line = ""
 
 numberTree()
 
-function minusFive() {
-    for(let i = 100; i > 0; i = i-5) {
-        console.log(i)
-    }
-
-}
-
-minusFive()
+// function minusFive() {
+//     for(let i = 100; i > 0; i = i-5) {
+//         console.log(i)
+//     }
+//
+// }
+//
+// minusFive()
--- JS/javascript practice.js
+++ JS/javascript practice.js
@@ -10,13 +10,25 @@
 //
 // console.log(cubeRoot(64))
 
-function remainder(input1,input2) {
-    if (Number(input1) && Number(input2)) {
-        return input1 % input2
-    } else {
-        return "This is not a number"
-    }
+// function remainder(input1,input2) {
+//     if (Number(input1) && Number(input2)) {
+//         return input1 % input2
+//     } else {
+//         return "This is not a number"
+//     }
+//
+// }
+//
+// console.log(remainder(9,2))
 
-}
-
-console.log(remainder(9,2))
+// function stars() {
+//     for (var i = 1; i <= 5; i++) {
+//         let line = ""
+//         for(var d = 1; d <= i; d++) {
+//             line = line.concat("*")
+//         }
+//         console.log(line)
+//     }
+// }
+//
+// stars()
