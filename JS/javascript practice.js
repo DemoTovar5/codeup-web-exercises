35503fe7a7ddcba4bd2b9a2209947292e0466b43
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-05-18 15:42:17 -0500

    May 18th

--- JS/javascript practice.js
+++ JS/javascript practice.js
@@ -31,4 +31,16 @@
 //     }
 // }
 //
+
 // stars()
+
+// Write a function named secondToLast(arr) that returns the data type of the second to last element of the array parameter
+//     When you call the function, pass in an array of your choosing
+
+// var myArray = ["one",2,3, "dog"]
+// function secondToLast(arr) {
+// return typeof arr[arr.length-2]
+//
+// }
+//
+// console.log(secondToLast(myArray));
