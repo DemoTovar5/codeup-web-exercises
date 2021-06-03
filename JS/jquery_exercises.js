5fae7fe8ec3ce1c9909db34b2dcc3058364b0386
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-06-03 16:19:54 -0500

    Jquery

--- JS/jquery_exercises.js
+++ JS/jquery_exercises.js
@@ -10,9 +10,36 @@ $(document).ready(function() {
 //
 // $(".FavSports").css("color", "Green");
 
-$(".Codeup").css("border", "solid", "blue");
+// $(".Codeup").css("border", "solid", "blue");
+//
+// $(" li ").css("font-size", "20px");
+//
+// var contents = $(' h1 ').html();
+// alert(contents);
 
-$(" li ").css("font-size", "20px");
 
-var contents = $(' h1 ').html();
-alert(contents);
+//
+$('#Hello').click(function(){
+$(this).css('background-color' , 'blue');
+});
+$('#Hello').dblclick(function(){
+   $(this).css('background-color' , '');
+});
+
+// $('#Hello').off('click').on('click',function(){
+//     $(this).css('background-color' , 'blue');
+// });
+
+$(' p ').dblclick(function() {
+ $(this) .css('font-size' , '18px');
+});
+
+$(' li ').hover(
+    function() {
+        $(this).css('color', 'blue');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
+
